7b7c9e97df75d9f5daccbe7dad1bfdfcd8bf039f
Spar9a <parfenuk.anton@gmail.com>
2016-12-22 22:18:41 +0300

    Почти полностью настроена первая страница

--- Активности/Активности/Pages/Necessary.xaml.cs
+++ Активности/Активности/Pages/Necessary.xaml.cs
@@ -23,84 +23,85 @@ namespace Активности.Pages
         public Necessary()
         {
             InitializeComponent();
-           // counter();
+            counter();
         }
         private void guild_10_plus_Click(object sender, RoutedEventArgs e)
         {
             guild_10.Value++;
-           // counter();
+            counter();
         }
 
         private void guild_10_minus_Click(object sender, RoutedEventArgs e)
         {
             guild_10.Value--;
-         //   counter();
+            counter();
         }
         private void guild_build_plus_Click(object sender, RoutedEventArgs e)
         {
             guild_build.Value++;
-          //  counter();
+            counter();
         }
 
         private void guild_build_minus_Click(object sender, RoutedEventArgs e)
         {
             guild_build.Value--;
-           // counter();
+            counter();
         }
         private void pirate_island_plus_Click(object sender, RoutedEventArgs e)
         {
             pirate_island.Value++;
-          //  counter();
+            counter();
         }
 
         private void pirate_island_minus_Click(object sender, RoutedEventArgs e)
         {
             pirate_island.Value--;
-           // counter();
+           counter();
         }
 
         private void pack_plus_Click(object sender, RoutedEventArgs e)
         {
             pack.Value++;
-            // counter();
+            counter();
         }
 
         private void pack_minus_Click(object sender, RoutedEventArgs e)
         {
             pack.Value--;
-            // counter();
+            counter();
         }
 
-        /*private void counter()
+        private void counter()
         {
             string guild_10_count = Convert.ToString(guild_10.Value) + " из " + Convert.ToString(guild_10.Maximum);
             string guild_build_count = Convert.ToString(guild_build.Value) + " из " + Convert.ToString(guild_build.Maximum);
             string pirate_island_count = Convert.ToString(pirate_island.Value) + " из " + Convert.ToString(pirate_island.Maximum);
+            string pack_count = Convert.ToString(pirate_island.Value) + " из " + Convert.ToString(pirate_island.Maximum);
             guild_10_counter.Dispatcher.BeginInvoke(new Action(delegate ()
             {
                 guild_10_counter.Content = guild_10_count;
-                if (guild_10.Value == guild_10.Maximum)
-                    guild_10_label.Background = guild_10_label_Green.Background;
-                else
-                    guild_10_label.Background = guild_10_label_Red.Background;
+              //  if (guild_10.Value == guild_10.Maximum)
+                   // guild_10_label.Background = guild_10_label_Green.Background;
+               // else
+                  //  guild_10_label.Background = guild_10_label_Red.Background;
             }));
             guild_build_counter.Dispatcher.BeginInvoke(new Action(delegate ()
             {
                 guild_build_counter.Content = guild_build_count;
-                if (guild_build.Value == guild_build.Maximum)
-                    guild_10_label1.Background = guild_10_label_Green.Background;
-                else
-                    guild_10_label1.Background = guild_10_label_Red.Background;
+               // if (guild_build.Value == guild_build.Maximum)
+                //    guild_10_label1.Background = guild_10_label_Green.Background;
+              //  else
+                 //   guild_10_label1.Background = guild_10_label_Red.Background;
             }));
             pirate_island_counter.Dispatcher.BeginInvoke(new Action(delegate ()
             {
                 pirate_island_counter.Content = pirate_island_count;
-                if (pirate_island.Value == pirate_island.Maximum)
-                    pirate_islandlabel.Background = guild_10_label_Green.Background;
-                else
-                    pirate_islandlabel.Background = guild_10_label_Red.Background;
+               // if (pirate_island.Value == pirate_island.Maximum)
+                 //   pirate_islandlabel.Background = guild_10_label_Green.Background;
+               // else
+                //    pirate_islandlabel.Background = guild_10_label_Red.Background;
             }));
-        }*/
+        }
 
     }
 }
