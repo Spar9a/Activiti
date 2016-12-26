9518b813fcbd7b20647df57b9463be9a41dc288d
Lance <Lance@World4Play.com>
2016-12-27 01:21:45 +0300

    всё писать в Roaming вместо Local

--- Активности/Activity/Properties/Settings.Designer.cs
+++ Активности/Activity/Properties/Settings.Designer.cs
@@ -38,6 +38,7 @@ namespace Activity.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SettingsManageabilityAttribute(global::System.Configuration.SettingsManageability.Roaming)]
         [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfAnyType xmlns:xsi=\"http://www.w3" +
             ".org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <" +
             "anyType />\r\n</ArrayOfAnyType>")]
